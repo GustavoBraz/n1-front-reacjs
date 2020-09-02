73838675719f178a10d6c1058f2d6ca67dfdb499
GustavoBraz <gustavo.braz89@gmail.com>
2020-09-01 22:19:51 -0300

    Front-end com ReactJS. Aula sobre Estado e Imutabilidade. Inclui função useState() para podermos clonar e alterar o estado de um objeto.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
+/*
+* useState retorna um array com duas posições
+* 
+* 1. Variável com o seu valor inicial
+* 2. Função para atualizarmos esse valor
+*/
+import React,{ useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web']);
+
+    function handleAddProject(){
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    };
+
     return (
         <>
-        <Header title="Homepage">
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-            </ul>
-        </Header>
-        <Header title="Projects">
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-                <li>Login</li>
-            </ul>
-        </Header>
+        <Header title="Projects" />
+        <ul>
+            {projects.map(project => <li key={project}>{project}</li>)}
+        </ul>
+
+        <button type="button" onClick={handleAddProject} >Adicionar projeto</button>
         </>
     );
 }
