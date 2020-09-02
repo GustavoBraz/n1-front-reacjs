799cada62ac11ec4af189ac723b659300aa129be
GustavoBraz <gustavo.braz89@gmail.com>
2020-09-01 21:45:40 -0300

    Front-end com ReactJS. Aula sobre Propriedades. Inclui o conceito de props e children no elementos.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App () {
-    return <h1>Hello World</h1>;
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+        <Header title="Homepage">
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+            </ul>
+        </Header>
+        <Header title="Projects">
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Login</li>
+            </ul>
+        </Header>
+        </>
+    );
 }
 
 export default App;
